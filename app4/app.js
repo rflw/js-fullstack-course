32dc9342b39ac55cbe1c15acc5db6cf7870c6626
Rafał Wawrzyniak <rflw@protonmail.com>
2023-02-04 16:45:45 +0100

    app4: setup view engine

--- app4/app.js
+++ app4/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+// import ejs from 'ejs';
 
 const app = express();
 
+// application settings
+// see https://expressjs.com/en/api.html#app.set
+// see https://expressjs.com/en/api.html#app.settings.table
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  // renders a view and sends the rendered HTML string to the client
+  res.render('home-guest');
 });
 
 app.listen(8000);
